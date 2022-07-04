c3817226d3afbacb727873ee6fec4634aab85745
Pablo <pablouboldi89@gmail.com>
2022-07-04 12:13:35 -0500

    Uncomment the db.create_all() line

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ ckeditor = CKEditor(app)
 Bootstrap(app)
 
 # # CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL1",  "sqlite:///blog.db")
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///blog.db"  # os.environ.get("DATABASE_URL1",  "sqlite:///blog.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
@@ -88,7 +88,7 @@ class Comment(Base, db.Model):
     parent_post = relationship("BlogPost", back_populates="comments")
 
 
-db.create_all()
+# db.create_all()
 
 
 # Esto es un decorador para que solo el admin pueda agregar posts, editarlos o borrarlos

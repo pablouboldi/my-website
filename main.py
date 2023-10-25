a40e0f316712828b947dc00a7068e98983f4449f
Pablo <pablouboldi89@gmail.com>
2023-10-25 11:38:10 +0100

    change imports

--- main.py
+++ main.py
@@ -1,38 +1,38 @@
-from flask import Flask, render_template
-from flask_bootstrap import Bootstrap
+import flask
+import flask_bootstrap
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
 app.config['SECRET_KEY'] = "8BYkEfBA6O6donzWlSihBXox7C0sKR6b"  # os.environ.get("SECRET_KEY")
-Bootstrap(app)
+flask_bootstrap.Bootstrap(app)
 
 
 @app.route('/')
 def home():
-    return render_template(
+    return flask.render_template(
         "index.html"
     )
 
 
 @app.route('/portfolio', methods=["GET", "POST"])
 def portfolio():
-    return render_template(
+    return flask.render_template(
         'portfolio.html'
     )
 
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    return flask.render_template("about.html")
 
 
 @app.route("/contact")
 def contact():
-    return render_template("contact.html")
+    return flask.render_template("contact.html")
 
 
 @app.route("/thanks", methods=['GET', 'POST'])
 def thanks():
-    return render_template("thanks.html")
+    return flask.render_template("thanks.html")
 
 
 if __name__ == "__main__":
